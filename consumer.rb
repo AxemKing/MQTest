f0512edb8fc6dc108b5c1b1ba7da2ec6936e3ff7
Thomas Rogers <ThomasRogers03@gmail.com>
2014-04-25 23:33:15 -0400

    Modified test consumer to keep logs (using exchange) and pull the hello queue. Cleans up properly

--- consumer.rb
+++ consumer.rb
@@ -1,6 +1,7 @@
 require 'bunny'
 require 'yaml'
 require 'pp'
+require 'thwait'
 
 conn = Bunny.new(hostname: 'ME')
 conn.start
@@ -10,14 +11,36 @@ q = ch.queue("", exclusive: true)
 x = ch.fanout("hello")
 q.bind(x)
 
-begin
-  q.subscribe(ack: true, block:  true) do |delivery_info, properties, body|
-    body = body.strip
-    pp " [x] Received #{body}"
-    pp YAML::load(body)
-    ch.ack(delivery_info.delivery_tag)
-  end
-rescue Interrupt => e
-  ch.close
-  conn.close
+threads = []
+
+threads << Thread.new do
+	begin
+	  q.subscribe(block:  true) do |delivery_info, properties, body|
+		body = body.strip
+		pp " [x] Received log: #{body}"
+		pp YAML::load(body)
+		ch.ack(delivery_info.delivery_tag)
+	  end
+	rescue Interrupt => e
+	  ch.close
+	end
 end
+
+ch2 = conn.create_channel
+q2 = ch2.queue("true")
+
+threads << Thread.new do
+	begin
+	  q2.subscribe(ack: true, block:  true) do |delivery_info, properties, body|
+		body = body.strip
+		pp " [x] Received #{body}"
+		pp YAML::load(body)
+		ch2.ack(delivery_info.delivery_tag)
+	  end
+	rescue Interrupt => e
+	  ch2.close
+	end
+end
+
+ThreadsWait.all_waits(threads)
+conn.close
