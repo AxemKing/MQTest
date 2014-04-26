fdf3e4d7b659bbdec049c09f13c87f2a8b30ef9a
AxemKing <AxemKing@gmail.com>
2014-04-25 23:23:11 -0400

    Test consumer now binds to hello fanout exchange

--- consumer.rb
+++ consumer.rb
@@ -6,11 +6,18 @@ conn = Bunny.new(hostname: 'ME')
 conn.start
 
 ch = conn.create_channel
-q = ch.queue("hello")
+q = ch.queue("", exclusive: true)
+x = ch.fanout("hello")
+q.bind(x)
 
-q.subscribe(ack: true, block:  true) do |delivery_info, properties, body|
-  body = body.strip
-  pp " [x] Received #{body}"
-  pp YAML::load(body)
-  ch.ack(delivery_info.delivery_tag)
+begin
+  q.subscribe(ack: true, block:  true) do |delivery_info, properties, body|
+    body = body.strip
+    pp " [x] Received #{body}"
+    pp YAML::load(body)
+    ch.ack(delivery_info.delivery_tag)
+  end
+rescue Interrupt => e
+  ch.close
+  conn.close
 end
