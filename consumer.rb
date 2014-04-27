458c83ccdbb21f145d60fdae0898f178886f68aa
AxemKing <AxemKing@gmail.com>
2014-04-27 01:11:32 -0400

    Redesigned test consumer

--- consumer.rb
+++ consumer.rb
@@ -3,50 +3,32 @@ require 'yaml'
 require 'pp'
 require 'thwait'
 
+class Consumer
+	def initialize(bunny_connection)
+		@ch = bunny_connection.create_channel
+		@q = @ch.queue("hello")
+	end
+
+	def consume
+		@q.subscribe(ack: true, block: true) do |delivery_info, properties, body|
+			@ch.ack(delivery_info.delivery_tag, true)
+		end
+	end
+end
+
 conn = Bunny.new(hostname: 'ME')
 conn.start
 
-mutex = Mutex.new
+consumers = []
+3.times do 
+	consumers << Consumer.new(conn)
+end
 
 threads = []
-stop = false
-
-threads << Thread.new do
-	ch = conn.create_channel
-	q = ch.queue("", exclusive: true)
-	x = ch.fanout("logs")
-	q.bind(x)
-	
-	q.subscribe(block: true) do |delivery_info, properties, body|
-		mutex.synchronize do
-			pp " [x] Received log: #{body}"
-		end
-		
-		delivery_info.consumer.cancel if stop
+consumers.each do |consumer|
+	threads << Thread.new do
+		consumer.consume
 	end
-	ch.close
 end
 
-threads << Thread.new do
-	ch2 = conn.create_channel
-	q2 = ch2.queue("hello")
-	
-	q2.subscribe(ack: true, block:  true) do |delivery_info, properties, body|
-		body = body.strip
-		mutex.synchronize do
-			pp YAML::load(body)
-		end
-		ch2.ack(delivery_info.delivery_tag)
-		
-		delivery_info.consumer.cancel if stop
-	end
-	ch2.close
-end
-
-begin
-	threads.each do |thread| thread.join end
-rescue Interrupt => e
-	threads.each do |thread| thread.join end
-end
-stop = true
-conn.close
+ThreadsWait.all_waits(threads)
