16a6d48495566c93951f2057651ac3c073c2696e
AxemKing <AxemKing@gmail.com>
2014-04-27 19:32:44 -0400

    More modifications to test consumer

--- consumer.rb
+++ consumer.rb
@@ -3,32 +3,80 @@ require 'yaml'
 require 'pp'
 require 'thwait'
 
-class Consumer
-	def initialize(bunny_connection)
-		@ch = bunny_connection.create_channel
-		@q = @ch.queue("hello")
+class ConsumerQueue
+	def initialize
+		@queue = Queue.new
+		@mutex = Mutex.new
+	end
+
+	def publish(data)
+		@mutex.synchronize do
+			@queue.push(data)
+		end
 	end
 
 	def consume
-		@q.subscribe(ack: true, block: true) do |delivery_info, properties, body|
-			@ch.ack(delivery_info.delivery_tag, true)
+		@mutex.synchronize do
+			@queue.pop
+		end
+	end
+
+	def empty?
+		@mutex.synchronize do
+			@queue.empty?
 		end
 	end
 end
 
+class Consumer
+	def initialize(bunny_connection)
+		@ch = bunny_connection.create_channel
+		@q = @ch.queue("hello")
+		@do_ack = false
+		@mutex = Mutex.new
+		@queue = ConsumerQueue.new
+	end
+
+	def consume
+		@q.subscribe(ack: true, block: true) do |delivery_info, properties, body|
+			@queue.publish(body)
+			if @do_ack
+				@ch.ack(delivery_info.delivery_tag, true)
+				@mutex.synchronize do
+					@do_ack = false
+				end
+			end
+		end
+	end
+
+	def ack
+		@mutex.synchronize do
+			@do_ack = true
+		end
+	end
+
+	def with_queue
+		yield @queue
+	end
+end	
+
 conn = Bunny.new(hostname: 'ME')
 conn.start
-
-consumers = []
-3.times do 
-	consumers << Consumer.new(conn)
-end
+ 
+consumer = Consumer.new(conn)
 
 threads = []
-consumers.each do |consumer|
-	threads << Thread.new do
-		consumer.consume
+threads << Thread.new do
+	while true
+		consumer.with_queue do |queue|
+			if !queue.empty?
+				while !queue.empty? do
+					puts "consuming"
+					queue.consume
+				end
+			end
+			consumer.ack
+		end
 	end
 end
-
-ThreadsWait.all_waits(threads)
+consumer.consume
